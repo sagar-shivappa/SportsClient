52ca31b9b6dc1e7a1348779a8e3e8c7cda9c7d26
sagar-shivappa <shivappasagar@gmail.com>
2023-11-02 19:14:24 +0530

    Build FIX

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideMenuComponent } from './side-menu/side-menu.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, SideMenuComponent],
+  declarations: [AppComponent, SideMenuComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
