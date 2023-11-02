9b09ced23cf5d8ab1c14c079a3a43760257f7586
sagar-shivappa <shivappasagar@gmail.com>
2023-11-02 20:46:37 +0530

    Basic for All categories done

--- src/shared/configurations/menu-items.ts
+++ src/shared/configurations/menu-items.ts
@@ -2,21 +2,26 @@ export const menu = [
   {
     activity: 'Registeration',
     link: '/register',
+    logo: '../../assets/registration_logo.png',
   },
   {
     activity: 'Games',
     link: '/games',
+    logo: '../../assets/games.png',
   },
   {
     activity: 'Categories',
     link: '/categories',
+    logo: '../../assets/categories.png',
   },
   {
     activity: 'Cricket',
     link: '/cricket',
+    logo: '../../assets/cricket_logo.png',
   },
   {
     activity: 'Admin Login',
     link: '/admin',
+    logo: '../../assets/admin.png',
   },
 ];
