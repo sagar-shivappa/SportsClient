f04099c70b557e6aecf758c53a90582731d7da8f
sagar-shivappa <shivappasagar@gmail.com>
2023-11-04 15:09:18 +0530

    Registration Form

--- src/app/registeration/register-form/register-form.component.ts
+++ src/app/registeration/register-form/register-form.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
-  styleUrls: ['./register-form.component.scss']
+  styleUrls: ['./register-form.component.scss'],
 })
-export class RegisterFormComponent {
+export class RegisterFormComponent implements OnInit {
+  registrationForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.registrationForm = this.fb.group({
+      fullName: new FormControl('', [Validators.required]),
+      age: new FormControl('', [Validators.required]),
+      gender: new FormControl('', [Validators.required]),
+      contactNumber: new FormControl('', [
+        Validators.required,
+        Validators.pattern('[789][0-9]{9}'),
+      ]),
+      cricket: new FormControl('', [Validators.required]),
+      cricketPlayerType: new FormControl(''),
+    });
+  }
+
+  ngOnInit() {}
+
+  onSubmit() {
+    console.log('submit', this.registrationForm.value);
+  }
 }
--- src/app/registeration/registeration.module.ts
+++ src/app/registeration/registeration.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RegisterationRoutingModule } from './registeration-routing.module';
 import { RegisterFormComponent } from './register-form/register-form.component';
 
-
 @NgModule({
-  declarations: [
-    RegisterFormComponent
-  ],
+  declarations: [RegisterFormComponent],
   imports: [
     CommonModule,
-    RegisterationRoutingModule
-  ]
+    RegisterationRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
-export class RegisterationModule { }
+export class RegisterationModule {}
