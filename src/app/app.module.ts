cf9bc12325bf5ac4574e44a19a1e44230062484e
sagar-shivappa <shivappasagar@gmail.com>
2023-11-08 15:16:47 +0530

    Addition of sinner feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideMenuComponent } from './side-menu/side-menu.component';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from './shared-module/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { LoaderInterceptor } from './interceptors/loader.interceptor';
+import { SpinnerComponent } from './shared-module/spinner/spinner.component';
 
 @NgModule({
   declarations: [AppComponent, SideMenuComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, HttpClientModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoaderInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/shared-module/shared.module.ts
+++ src/app/shared-module/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeaturesComponent } from './features/features.component';
+import { SpinnerComponent } from './spinner/spinner.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
-  declarations: [FeaturesComponent],
-  imports: [CommonModule],
-  exports: [FeaturesComponent],
+  declarations: [FeaturesComponent, SpinnerComponent],
+  imports: [CommonModule, MatProgressSpinnerModule],
+  exports: [FeaturesComponent, SpinnerComponent],
 })
 export class SharedModule {}
