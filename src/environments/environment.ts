4b754902981937c5c67aefb9317f25cc8abfd526
Sagar Shivappa <62869763+sagar-shivappa@users.noreply.github.com>
2024-11-14 23:01:45 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,8 @@
 import { environmentCommon } from '../environments/environment.common';
 
 const host = 'https://devangasports.onrender.com';
-const api_key = 'rzp_live_jbbbhKCqO19hMk';
+//const api_key = 'rzp_live_jbbbhKCqO19hMk';
+const api_key = 'rzp_test_fFPfAbmashyfLE';
 
 export const environment = {
   ...environmentCommon(host, api_key),
