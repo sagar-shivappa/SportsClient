a3c79e86ba421a081f699eeaa84e6a5e55fd415b
sagar-shivappa <shivappasagar@gmail.com>
2025-10-25 11:42:10 +0530

    code clean up, successful run

--- src/app/registeration/registeration.module.ts
+++ src/app/registeration/registeration.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RegisterationRoutingModule } from './registeration-routing.module';
 import { RegisterFormComponent } from './register-form/register-form.component';
-import { GooglePayButtonModule } from '@google-pay/button-angular';
-
 import { RazorpayPaymentComponent } from './razorpay-payment/razorpay-payment.component';
 import { CricketHomeComponent } from '../cricket/cricket-home/cricket-home.component';
 import { KidsCategoryComponent } from '../category/kids-category/kids-category.component';
@@ -20,7 +18,6 @@ import { KidsCategoryComponent } from '../category/kids-category/kids-category.c
     RegisterationRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    GooglePayButtonModule,
     KidsCategoryComponent,
   ],
 })
